198c93984172f8ff52def6091574ccf649d43365
Daniel Schroeder <kriegerfaust88@gmail.com>
2020-05-01 16:13:24 -0700

    Initial commit

---
+++ src/main/java/com/dschroeder/tagcaddy/TagcaddyApplication.java
@@ -0,0 +1,13 @@
+package com.dschroeder.tagcaddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagcaddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TagcaddyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dschroeder/tagcaddy/TagcaddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dschroeder.tagcaddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagcaddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
